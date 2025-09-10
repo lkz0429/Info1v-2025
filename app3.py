65153d6990ea8fcc25f882d952be9ef68edca0ba
lkz0429 <marciolucascmatos@gmail.com>
2025-09-10 17:51:28 -0300

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,7 @@
+num1 = int(input("digite uma nota:"))
+num2 = int(input("digite uma nota:"))
+num3 = int(input("digite uma nota:"))
+num4 = int(input("digite uma nota:"))
+soma = num1 + num2 + num3 + num4
+media = soma/4
+print(media)
